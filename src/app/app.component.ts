52ee303a819e5f7e038cc26e16090691086640da
Yilber Toledo <26316784+yilbertoledo@users.noreply.github.com>
2020-06-22 03:56:12 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng-pipes';
+  nombre = 'Yilber toledo';
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  pi = Math.PI;
+  porcentaje = 0.234;
+  salario = 1234.5;
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: { calle: 'Primera', casa: '20' },
+  };
+
+  valorPromesa = new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve('Data loaded');
+    }, 5000);
+  });
+
+  idioma = 'en';
+
+  fecha = new Date();
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
 
+// Localization
+import localEsCl from '@angular/common/locales/es-CL';
+registerLocaleData(localEsCl);
+
+import localFr from '@angular/common/locales/fr';
+registerLocaleData(localFr);
+
+// Components
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [{ provide: LOCALE_ID, useValue: 'es-CL' }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
